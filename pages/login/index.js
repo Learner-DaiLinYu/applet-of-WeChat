eea48c5cd2cd2660d2d7fe8bb2dfb230f36cbb27
Learner <1181434433@qq.com>
2022-05-06 12:51:18 +0800

    登入验证

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,8 +1,67 @@
 // pages/login/index.js
 Page({
     data: {
-
+      islogin:true,
+      getUserinfo:false,
+      sex:null
     },
+    onLoad(){
+      wx.getStorage({
+        key:'userInfo',
+        success:res=>{
+          if(res.data){
+            wx.request({
+              url: 'http://localhost:8080/login',
+              method:'POST',
+              data:{
+                'UserInfo':res.data
+              },
+              success:res=>{
+                wx.showToast({
+                  title: res.data,
+                  icon:'success'
+                })
+              }
+            })
+            this.setData({
+              islogin:false,
+              getUserinfo:true,
+              sex:res.data.gender
+            })
+          }
+        }
+      })
+    },
+    getUsermessage(){
+       wx.getUserProfile({
+         desc: 'desc',
+         success:res=>{
+          wx.request({
+            url: 'http://localhost:8080/login',
+            method:'POST',
+            data:{
+              'UserInfo':res.userInfo
+            },
+            success:res=>{
+              wx.showToast({
+                title: res.data,
+                icon:'success'
+              })
+            }
+          })
+           wx.setStorage({
+             key:'userInfo',
+             data:res.userInfo,
+           })
+          this.setData({
+            getUserinfo:true,
+            sex:res.userInfo.gender,
+            islogin:false
+          })
+         }
+       })
+    },
+  
     login(){
         wx.login({
           success:res=>{
